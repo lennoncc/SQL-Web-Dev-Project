bfa45f38b8f44b12f557ff2da62e08d98233635a
lennoncc <lencruz@ucdavis.edu>
2024-07-16 22:29:53 -0700

    PI-9 Followed https://www.geeksforgeeks.org/build-a-simple-web-app-with-express-angular/, learned how to create get http get request in angular by creating a service (found out this is unnecessary when following ag-grid guide :) )

--- project-frontend/src/app/app.component.ts
+++ project-frontend/src/app/app.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { SalespersonsComponent } from './salespersons/salespersons.component';
 import { AggridComponent } from './aggrid/aggrid.component';
-
+import { ApiService } from './api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SalespersonsComponent, AggridComponent],
+  imports: [RouterOutlet, HeaderComponent, SalespersonsComponent, AggridComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'project-frontend';
+  salespersons: any;
+  constructor(private apiService: ApiService) { };
+  ngOnInit(): void {
+    this.apiService.getMessage().subscribe(data => {
+      this.salespersons = data;
+    });
+  }
 }
